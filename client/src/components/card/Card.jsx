6e9bf31495e3de850b14bdfd5b7c5924f239f21d
Vivi Schulz <haweirh@gmail.com>
2023-01-04 22:48:43 -0800

    Completed feature api call with strapi

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -7,16 +7,30 @@ const Card = ({item}) => {
     <Link to={`product/${item.id}`}>
       <div className="card">
         <div className="card__image">
-          {item.isNew && <span>New Season</span>}
-          <img src={item.img} alt="" className="mainImg"></img>
-          <img src={item.img2} alt="" className="secondImg"></img>
+          {item?.attributes.isNew && <span>New Season</span>}
+          <img
+            src={
+              process.env.REACT_APP_UPLOAD_URL +
+              item.attributes?.img?.data?.attributes?.url
+            }
+            alt=""
+            className="mainImg"
+          ></img>
+          <img
+            src={
+              process.env.REACT_APP_UPLOAD_URL +
+              item.attributes?.img2?.data?.attributes?.url
+            }
+            alt=""
+            className="secondImg"
+          ></img>
         </div>
 
-        <h2 className='card__title'>{item.title}</h2>
+        <h2 className="card__title">{item?.attributes.title}</h2>
 
         <div className="card__price">
-          <h3>${item.oldPrice}</h3>
-          <h3>${item.price}</h3>
+          <h3>${item.oldPrice || item?.attributes.price + 20}</h3>
+          <h3>${item?.attributes.price}</h3>
         </div>
       </div>
     </Link>

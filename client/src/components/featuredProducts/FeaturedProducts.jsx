a821cf23128636853ba4aa013f63d74876cbed2e
Vivi Schulz <haweirh@gmail.com>
2023-01-04 22:48:43 -0800

    Completed feature api call with strapi

--- client/src/components/featuredProducts/FeaturedProducts.jsx
+++ client/src/components/featuredProducts/FeaturedProducts.jsx
@@ -1,43 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from "../card/Card";
 import "./FeaturedProducts.scss";
+import axios from "axios";
 
 const FeaturedProducts = ({type}) => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/1192607/pexels-photo-1192607.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Man Contrast Paneled Shirt",
-      isNew: true,
-      oldPrice: 32,
-      price: 23,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Flap Pocket Denim Jacket",
-      isNew: true,
-      oldPrice: 54,
-      price: 47,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/3193731/pexels-photo-3193731.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Letter Embroidery Waffle Joggers",
-      isNew: false,
-      oldPrice: 63,
-      price: 45,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/4890733/pexels-photo-4890733.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Crew Neck Sweater",
-      isNew: false,
-      oldPrice: 82,
-      price: 64,
-    },
-  ];
+  // const data = [
+  //   {
+  //     id: 1,
+  //     img: "https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //     img2: "https://images.pexels.com/photos/1192607/pexels-photo-1192607.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //     title: "Man Contrast Paneled Shirt",
+  //     isNew: true,
+  //     oldPrice: 32,
+  //     price: 23,
+  //   },
+  //   {
+  //     id: 2,
+  //     img: "https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //     title: "Flap Pocket Denim Jacket",
+  //     isNew: true,
+  //     oldPrice: 54,
+  //     price: 47,
+  //   },
+  //   {
+  //     id: 3,
+  //     img: "https://images.pexels.com/photos/3193731/pexels-photo-3193731.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //     title: "Letter Embroidery Waffle Joggers",
+  //     isNew: false,
+  //     oldPrice: 63,
+  //     price: 45,
+  //   },
+  //   {
+  //     id: 4,
+  //     img: "https://images.pexels.com/photos/4890733/pexels-photo-4890733.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //     title: "Crew Neck Sweater",
+  //     isNew: false,
+  //     oldPrice: 82,
+  //     price: 64,
+  //   },
+  // ];
+
+const [data, setData] = useState([])
+
+useEffect(()=>{
+const fetchData = async ()=>{
+
+  try{
+    const res = await axios.get(
+      process.env.REACT_APP_API_URL + "/products?populate=*",
+      {
+        headers: {
+          Authorization: "bearer" + process.env.REACT_APP_API_TOKEN,
+        },
+      }
+    );
+    setData(res.data.data);
+  }catch(err){
+    console.log(err)
+  }
+};
+
+fetchData();
+}, [])
+
+console.log(data);
 
   return (
     <div className="featured-products">

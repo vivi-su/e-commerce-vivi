b8c8a141ecfc9a9fce01ffcd90b704994f2fe7f4
Vivi Schulz <haweirh@gmail.com>
2023-01-06 21:52:22 -0800

    Added redux persistor to ramain cart quantity after refreshing the page

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {store} from "./redux/store";
+import {store, persistor} from "./redux/store";
 import {Provider} from "react-redux";
+import { PersistGate} from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={"loading"} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );

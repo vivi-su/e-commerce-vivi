dc35dba5777ba133832ba09cd87d25f39a9ccdb2
Vivi Schulz <haweirh@gmail.com>
2023-01-11 10:13:16 -0800

    Modified home page for type

--- client/src/components/featuredProducts/FeaturedProducts.jsx
+++ client/src/components/featuredProducts/FeaturedProducts.jsx
@@ -4,41 +4,6 @@ import "./FeaturedProducts.scss";
 import useFetch from "../../hooks/useFetch";
 
 const FeaturedProducts = ({type}) => {
-  // const data = [
-  //   {
-  //     id: 1,
-  //     img: "https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  //     img2: "https://images.pexels.com/photos/1192607/pexels-photo-1192607.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  //     title: "Man Contrast Paneled Shirt",
-  //     isNew: true,
-  //     oldPrice: 32,
-  //     price: 23,
-  //   },
-  //   {
-  //     id: 2,
-  //     img: "https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  //     title: "Flap Pocket Denim Jacket",
-  //     isNew: true,
-  //     oldPrice: 54,
-  //     price: 47,
-  //   },
-  //   {
-  //     id: 3,
-  //     img: "https://images.pexels.com/photos/3193731/pexels-photo-3193731.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  //     title: "Letter Embroidery Waffle Joggers",
-  //     isNew: false,
-  //     oldPrice: 63,
-  //     price: 45,
-  //   },
-  //   {
-  //     id: 4,
-  //     img: "https://images.pexels.com/photos/4890733/pexels-photo-4890733.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  //     title: "Crew Neck Sweater",
-  //     isNew: false,
-  //     oldPrice: 82,
-  //     price: 64,
-  //   },
-  // ];
 
 const {data, loading, error} = useFetch(`/products?populate=*&[filters][type][$eq]=${type}`);
 

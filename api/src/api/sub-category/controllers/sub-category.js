8227e0be05a0dcd7e6808663ca85277463b07e61
Vivi Schulz <haweirh@gmail.com>
2023-01-11 10:16:45 -0800

    Delete api directory

--- api/src/api/sub-category/controllers/sub-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sub-category controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::sub-category.sub-category');
--- api/src/api/sub-category/routes/sub-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sub-category router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::sub-category.sub-category');

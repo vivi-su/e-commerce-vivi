108079df13bc580fb12735d9acdb5b37ccf5d3f5
Vivi Schulz <haweirh@gmail.com>
2023-01-11 10:16:45 -0800

    Delete api directory

--- api/src/api/sub-category/services/sub-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sub-category service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::sub-category.sub-category');
